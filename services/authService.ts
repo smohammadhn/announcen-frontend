c2d5002c641bc10cb821803316cb437bc097f3e5
smohammadhn <smohammadhnd@gmail.com>
2023-12-08 22:22:19 +0200

    feat(services): build auth service

---
+++ services/authService.ts
@@ -0,0 +1,49 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
+import axios from '@/services/apiClient'
+import { useMutation } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+
+interface User {
+  email: string
+  password: string
+}
+
+const authService = {
+  login(onSuccess?: () => void) {
+    return useMutation<User, AxiosError<ErrorMessage>, User>({
+      mutationFn: (user: User) =>
+        axios.post<User>('/auth', user).then((res) => res.data),
+      onSuccess,
+    })
+  },
+
+  register(onSuccess: (savedUser: User) => void) {
+    return useMutation<User, AxiosError<ErrorMessage>, User>({
+      mutationFn: (user: User) =>
+        axios.post<User>('/users', user).then((res) => res.data),
+      onSuccess,
+    })
+  },
+
+  async verifyToken(authToken: string) {
+    // TODO: make sure that the authToken variable contains a valid base64-encoded JWT token (object). and not any malicious input
+
+    return fetch('http://localhost:8000/api/auth/verify', {
+      method: 'POST',
+      headers: {
+        Cookie: `auth-token=${authToken}`,
+      },
+    }).then((res) => {
+      res.status
+
+      if (res.status === 200) {
+        return true
+      }
+
+      return false
+    })
+  },
+}
+
+export default authService
