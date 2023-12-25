f874bff345523bd42005d628bcf29700968b8e66
smohammadhn <smohammadhnd@gmail.com>
2023-12-25 03:58:56 +0330

    chore(contact): bring back boilerplate

--- app/(layoutDefault)/dashboard/contact/page.tsx
+++ app/(layoutDefault)/dashboard/contact/page.tsx
@@ -1,27 +1,3 @@
-'use client'
-
-import { Button } from '@/components/ui/button'
-import { ToastAction } from '@/components/ui/toast'
-import { useToast } from '@/components/ui/use-toast'
-
 export default function Contact() {
-  const { toast } = useToast()
-
-  return (
-    <div>
-      <Button
-        variant="outline"
-        onClick={() => {
-          toast({
-            variant: 'destructive',
-            title: 'Uh oh! Something went wrong.',
-            description: 'There was a problem with your request.',
-            action: <ToastAction altText="Try again">Try again</ToastAction>,
-          })
-        }}
-      >
-        Show Toast
-      </Button>
-    </div>
-  )
+  return <div>Contact page</div>
 }
