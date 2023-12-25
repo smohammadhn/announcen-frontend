fda9443fb280a6df1fc755bbd3dfc5735d9bc577
smohammadhn <smohammadhnd@gmail.com>
2023-12-25 03:47:40 +0330

    fix(dashboard): no way on mobile to see ann details

--- components/cards/CardAnnouncement/index.tsx
+++ components/cards/CardAnnouncement/index.tsx
@@ -1,5 +1,5 @@
-import './index.scss'
 import { Button } from '@/components/ui/button'
+import './index.scss'
 
 import Image from 'next/image'
 import Link from 'next/link'
@@ -11,13 +11,9 @@ interface Props {
 export default function CardAnnouncement({ item }: Props) {
   return (
     <div className="card-announcement">
-      <Image
-        className="card-announcement__image"
-        src="/images/placeholder-avatar.jpg"
-        width={100}
-        height={100}
-        alt="website logo"
-      />
+      <Link className="card-announcement__image" href={`/dashboard/announcements/${item._id}`}>
+        <Image src="/images/placeholder-avatar.jpg" width={100} height={100} alt="website logo" />
+      </Link>
 
       <h3 className="card-announcement__title">{`${item.firstName} ${item.lastName}`}</h3>
       <p className="card-announcement__subtitle">From: {item.city}</p>
